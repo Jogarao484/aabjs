dd695c0dcba67c1094e44527f90ce1969a29be30
yogesh sharma <yogesh sharma@yogeshsharma-PC>
2016-04-03 19:24:55 +0530

    class added and main method added

--- src/com/gcompany/hangandhave/designpatterns/DummyClass.java
+++ src/com/gcompany/hangandhave/designpatterns/DummyClass.java
@@ -1,5 +1,7 @@
 package com.gcompany.hangandhave.designpatterns;
 
 public class DummyClass {
-
+public static void main(String[] args) {
+	
+}
 }
---
+++ src/com/gcompany/hangandhave/designpatterns/Sample.java
@@ -0,0 +1,5 @@
+package com.gcompany.hangandhave.designpatterns;
+
+public class Sample {
+
+}
