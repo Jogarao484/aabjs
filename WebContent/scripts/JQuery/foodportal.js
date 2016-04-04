464bf83458ed4852d4074fb986c262af90618e28
joga <joga@Jogarao.aecsgroup.in>
2016-04-04 13:06:25 -0500

    Signed-off-by: joga <joga@Jogarao.aecsgroup.in>

--- WebContent/scripts/JQuery/foodportal.js
+++ WebContent/scripts/JQuery/foodportal.js
@@ -1,3 +1,11 @@
+/*For Food Amount Calculations*/
 $(document).ready(function(){
+	$("foodAmtPlus").click(function(){
+		var foodAmount = $(this).val();
+	});
 	
+	$("foodAmtMinus").click(function(){
+		var foodAmount = $(this).val();
+	});
 });
+/* End Code For Food Amount Calculations*/
